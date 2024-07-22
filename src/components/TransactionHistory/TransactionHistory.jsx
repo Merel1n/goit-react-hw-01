93785f0ec90fc16312b731f2e231fc7b0230070f
Inna Matviienko <ineskamit@gmail.com>
2024-07-23 00:20:58 +0200

    update TransactionHistory

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -10,15 +10,17 @@ const TransactionHistory = ({ items }) => {
                 </tr>
             </thead>
 
-            {items.map((item) => (
-                <tbody key={item.id}>
+            <tbody key={items.id}>
+                {items.map((item) => (
+                    <>
                     <tr className={css.item}>
                         <td>{item.type}</td>
                         <td>{item.amount}</td>
                         <td>{item.currency}</td>
-                    </tr>
-                </tbody>
+                        </tr>
+                    </>
             ))}
+        </tbody>
         </table>
 
     )
